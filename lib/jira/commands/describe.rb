69ecf553d2db7c6d1514cb932d9b7e32d77ef447
swerling <swerlingbiz@gmail.com>
2017-12-20 01:08:27 +0000

    add description to describe command
    - and make summary and desc cutoff configurable

--- lib/jira/commands/describe.rb
+++ lib/jira/commands/describe.rb
@@ -2,8 +2,11 @@ module Jira
   class CLI < Thor
 
     desc "describe <ticket>", "Describes the input ticket"
+    method_option :summary_limit, aliases: "-s", type: :numeric, default: 50, banner: "Summary cutoff"
+    method_option :description_limit, aliases: "-d", type: :numeric, default: 100, banner: "Description cutoff"
+
     def describe(ticket)
-      Command::Describe.new(ticket).run
+      Command::Describe.new(ticket, options).run
     end
 
   end
@@ -11,10 +14,11 @@ module Jira
   module Command
     class Describe < Base
 
-      attr_accessor :ticket
+      attr_accessor :ticket, :options
 
-      def initialize(ticket)
+      def initialize(ticket, options)
         self.ticket = ticket
+        self.options = options
       end
 
       def run
@@ -24,11 +28,11 @@ module Jira
       end
 
       def header
-        [ 'Ticket', 'Assignee', 'Status', 'Summary' ]
+        [ 'Ticket', 'Assignee', 'Status', 'Summary', 'Description' ]
       end
 
       def row
-        [ ticket, assignee, status, summary ]
+        [ ticket, assignee, status, summary, description ]
       end
 
       def errors
@@ -43,8 +47,20 @@ module Jira
         (fields['status'] || {})['name'] || 'Unknown'
       end
 
+      def summary_limit
+        options['summary_limit']
+      end
+
       def summary
-        truncate(json['fields']['summary'], 45)
+        truncate(json['fields']['summary'], self.summary_limit)
+      end
+
+      def description_limit
+        options['description_limit']
+      end
+
+      def description
+        truncate(json['fields']['description'], self.description_limit)
       end
 
       def fields
